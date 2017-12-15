7e2c4d55b2a1fbbcaaa11190fd7bd615ffe52ff6
Bojan Knezevic <bojan.knezevic@diwanee.com>
2017-12-15 14:58:40 +0100

    css hot loader, update README

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,34 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 // ----- Output file paths
-var cssOutput = 'css/master.css',
+var outputDir = 'html/',
+    cssOutput = 'css/master.css',
     jsOutput = 'js/master.js';
 
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'html'),
+        path: path.resolve(__dirname, outputDir),
         filename: jsOutput
     },
     devtool: 'source-maps',
     module: {
         rules: [
             // ----- SCSS compiling
-            { test: /\.scss$/, use: ExtractTextPlugin.extract({
+            { test: /\.scss$/, use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
                     fallback: "style-loader",
                     use: [
-                        {loader: "css-loader", options: { importLoaders: 1 } },
+                        { loader: "css-loader", options: { importLoaders: 1 } },
                         "postcss-loader",
                         "sass-loader"
                     ]
-                })
+                }))
             }
         ]
     },
     devServer: {
         // ----- Webpack dev server options
-        contentBase: path.join(__dirname, "html"),
+        contentBase: path.join(__dirname, outputDir),
         compress: true,
         port: 3000,
         stats: 'errors-only',
